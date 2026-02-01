7bcfcd413708dcf217338b212a447fc975b39f9d
himanshupandey153-source <himanshupandey153@gmail.com>
2026-02-01 15:21:28 +0530

    Add database schema

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    course VARCHAR(100),
+    year INT
+);
